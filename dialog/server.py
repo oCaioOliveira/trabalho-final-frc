ce6e05fa00917aeb4e233d0d408b064e3591945f
oCaioOliveira <caiocesar31951910@hotmail.com>
2023-12-16 15:48:07 -0300

    Add initial version of app dialog to comunicate only with text

---
+++ dialog/server.py
@@ -0,0 +1,26 @@
+import asyncio
+import websockets
+
+salas = {}
+
+async def server(websocket, path):
+    sala = path.strip("/")
+
+    if sala not in salas:
+        salas[sala] = set()
+
+    salas[sala].add(websocket)
+
+    try:
+        async for message in websocket:
+            for client in salas[sala]:
+                if client != websocket:
+                    await client.send(message)
+    finally:
+        salas[sala].remove(websocket)
+
+
+start_server = websockets.serve(server, "localhost", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
