33932a6b5e00fb3005955041849ae8c92aa82c98
oCaioOliveira <caiocesar31951910@hotmail.com>
2023-12-17 16:38:11 -0300

    Add style and messages

--- dialog/server.py
+++ dialog/server.py
@@ -21,7 +21,7 @@ async def server(websocket, path):
         salas[sala].remove(websocket)
 
 
-start_server = websockets.serve(server, "ip-desejado", 8765)
+start_server = websockets.serve(server, "192.168.0.112", 8765)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
