294186ef24b3f8c574b8dca83a56a41d66902348
oCaioOliveira <caiocesar31951910@hotmail.com>
2023-12-17 23:15:42 -0300

    Add new feature to list existing rooms

--- dialog/server.py
+++ dialog/server.py
@@ -1,5 +1,6 @@
 import asyncio
 import websockets
+import json
 
 salas = {}
 
@@ -21,7 +22,14 @@ async def server(websocket, path):
         salas[sala].remove(websocket)
 
 
+async def listar_salas_disponiveis(websocket, path):
+    salas_disponiveis = list(salas.keys())
+    await websocket.send(json.dumps(salas_disponiveis))
+
+
 start_server = websockets.serve(server, "192.168.0.112", 8765)
+start_listar_salas = websockets.serve(listar_salas_disponiveis, "192.168.0.112", 8766)
 
 asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_until_complete(start_listar_salas)
 asyncio.get_event_loop().run_forever()
